be59b09f855a7e7201e81d9a8b715a98452e4cb8
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-06-25 08:30:33 +0300

    feat: add tables category

--- demo/src/catalog/mycatalog.js
+++ demo/src/catalog/mycatalog.js
@@ -12,6 +12,7 @@ for( let x in Lines ) catalog.registerElement( Lines[x] );
 for( let x in Holes ) catalog.registerElement( Holes[x] );
 for( let x in Items ) catalog.registerElement( Items[x] );
 
+catalog.registerCategory('tables', 'Tables', [Items.canteenTableWith4Chairs, Items.canteenTableWith8Chairs, Items.canteenRoundedTableWith4Chairs] );
 catalog.registerCategory('windows', 'Windows', [Holes.window, Holes.sashWindow, Holes.venetianBlindWindow, Holes.windowCurtain] );
 catalog.registerCategory('doors', 'Doors', [Holes.door, Holes.doorDouble, Holes.panicDoor, Holes.panicDoorDouble, Holes.slidingDoor] );
 
